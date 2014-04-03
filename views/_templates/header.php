2e9fd7d2efd2de3466cbcfc996414ca5b36aa482
Rolf Isler <isler.rolf@gmail.com>
2014-04-03 16:40:28 +0200

    added Google Analytics

--- views/_templates/header.php
+++ views/_templates/header.php
@@ -19,7 +19,23 @@
         <script src="<?php echo URL; ?>public/js/jquery.scrollTo.min.js" type="text/javascript"></script>
         <script src="<?php echo URL; ?>public/js/jquery.easing.1.3.js" type="text/javascript"></script>
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+        <script>
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
+            ga('create', 'UA-49674561-1', 'pfadiorion.ch');
+            ga('send', 'pageview');
+
+        </script>
         <!-- Less compiler -->
         <?php
         $url = isset($_GET['url']) ? $_GET['url'] : null;
@@ -44,22 +60,22 @@
         <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>public/js/parallax/parallax-skeleton.css" />
         <script src="<?php echo URL; ?>public/js/parallax/jquery.parallax-skeleton.js"></script>
         <script>
-            (function() {
-                document.getServerUrl = function() {
-                    return "<?php echo URL; ?>";
-                };
+    (function() {
+        document.getServerUrl = function() {
+            return "<?php echo URL; ?>";
+        };
 
-                colors = {
-                    red: '#CC3D18',
-                    violet: '#4710B5',
-                    white: '#FFF',
-                    black: '#000'
-                }
-            }());
+        colors = {
+            red: '#CC3D18',
+            violet: '#4710B5',
+            white: '#FFF',
+            black: '#000'
+        }
+    }());
 
-            $(window).load(function() {
-                //$('body, html').css({scrollTop: 0});
-            });
+    $(window).load(function() {
+        //$('body, html').css({scrollTop: 0});
+    });
         </script>
         <style>
             #logged_in_panel{
