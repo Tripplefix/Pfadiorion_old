b3a74c25367271d6cc19cb102844c47c4e21fbff
Rolf Isler <isler.rolf@gmail.com>
2014-07-03 14:39:48 +0200

    online shop initialized

---
+++ controllers/shop.php
@@ -0,0 +1,12 @@
+<?php
+
+class Shop extends Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function index() {
+        $this->view->render('shop/index');
+    }
+}
---
+++ views/shop/index.php
@@ -0,0 +1,25 @@
+<script>
+    $(function() {
+        
+    });
+</script>
+<style>
+    /*#main_container{
+        width: 
+    }*/
+    
+    
+</style>
+
+<div id="main_container">
+    <div id="item_list">
+        <div class="category">
+            <div class="category_title"></div>
+            <div id="item_01" class="shop_item">
+                <div class="item_thumb"></div>
+                <h3 class="item_title">Orion Foulard</h3>
+                <div class="item_info"></div>
+            </div>
+        </div>
+    </div>
+</div>
